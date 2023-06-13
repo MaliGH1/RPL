2125be5b41c454981efb4680073a12c16d7c19b2
Jafarrr25 <fajaralnajiim09@gmail.com>
2023-06-13 20:58:18 +0700

    commit

---
+++ app/Controllers/Login.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace App\Controllers;
+
+class Login extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+    public function register()
+    {
+        
+    }
+
+    public function login()
+    {
+        
+    }
+}
---
+++ app/Controllers/Mobil.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace App\Controllers;
+
+class Mobil extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+}
